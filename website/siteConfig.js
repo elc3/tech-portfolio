99a647da9af6d8c6a409cac258b075cc7a1216ff
Emily Cunningham <49571790+elc3@users.noreply.github.com>
2020-10-02 15:24:52 -0400

    Beginning technical portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Emily Cunningham', // Title for your website.
+    tagline: 'vGHC20 attendee!',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://elc3.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'elc3',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -43,8 +43,8 @@ const siteConfig = {
 
     /* Colors for website */
     colors: {
-        primaryColor: '#2C312E',
-        secondaryColor: '#72C3E7',
+        primaryColor: '#b0384c',
+        secondaryColor: '#636363',
     },
 
     // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'emily-cunningham-usc',
         twitter: '',
-        github: '',
+        github: 'elc3',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
